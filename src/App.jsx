11b385ff16918df2d835249e183bdcb0e0f5708c
Ваше ім'я < email>
2025-03-09 17:50:27 +0200

    licaleStor add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
@@ -10,21 +10,34 @@ const initContacts = [
   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 ];
+const localStorageKAY = "contacts";
 
 function App() {
-  const [contacts, setContacts] = useState(initContacts);
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => [...prevContacts, newContact]);
-  };
+  const [contacts, setContacts] = useState(() =>{
+    const sevedContact = localStorage.getItem('contacts');
+    
+    return sevedContact ? JSON.parse(sevedContact) : initContacts
+  });
+  
   const [searchName, setSearchName] = useState("");
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase())
+  
   );
-
+  
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
   const deleteContact = (contactId) =>
     setContacts((prevContacts) =>
       prevContacts.filter((contact) => contact.id !== contactId)
     );
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts]);
+
+ 
 
   return (
     <>
